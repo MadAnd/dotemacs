a030f64874625afbe9b37b7d8b8641eab632be88
MadAnd <dev@madand.net>
2015-06-24 05:47:18 +0300

    Add actual config

---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -e
+
+DOTSPACEMACS=~/.spacemacs
+GIT_REPO="https://github.com/MadAnd/dotemacs.git"
+INSTALL_DIR=~/.emacs
+SPACEMACS_REPO="https://github.com/MadAnd/spacemacs.git"
+SPACEMACS_DIR=~/.emacs.d
+
+# USAGE
+#   find_executable executable-name
+# DESCRIPTION
+#   Find an executable, given as a first argument, in PATH
+#   and assign the found path to a global variable.
+# EXAMPLES
+#  `find_executable git` - will set the variable GIT_CMD to e.g. '/usr/bin/git'
+#  `find_executable bash` - will set the variable BASH_CMD, etc.
+find_executable()
+{
+    if (( $# != 1 )); then
+        echo 'USAGE: find_executable executable-name' >&2
+        return 1
+    fi
+
+    local cmd_executable=$(which $1 2>/dev/null)
+
+    if [ -z "$cmd_executable" ]; then
+        echo "$1 not found in PATH" >&2
+        return 2
+    fi
+
+    local cmd_var_name=${1^^}_CMD
+    declare -g $cmd_var_name="$cmd_executable"
+}
+
+# USAGE
+#   backup_place path
+# DESCRIPTION
+#   Rename the target file or diretory by adding the ".bak" prefix
+#   or delete it, if it is a symlink.
+# EXAMPLES
+#   `backup_place ~/.config` - will rename it to `~/.config.bak`
+backup_place()
+{
+    if (( $# != 1 )); then
+        echo 'USAGE: backup_place path' >&2
+        return 1
+    fi
+
+    if [[ -h "$1" ]]; then
+        # if symlink - just remove it
+        rm "$1"
+    elif [[ -e "$1" ]]; then
+        # if file or dir - move to the same path with ".bak" suffix
+        mv -f "$1" "$1".bak
+    fi
+}
+
+# Generate EMACS_CMD
+find_executable emacs
+# Generate GIT_CMD
+find_executable git
+
+# Backup things we are going to replace
+backup_place "$DOTSPACEMACS" 
+backup_place "$INSTALL_DIR" 
+backup_place "$SPACEMACS_DIR" 
+
+# Install the config
+$GIT_CMD clone "$GIT_REPO" "$INSTALL_DIR"
+ln -sf "$INSTALL_DIR/dotspacemacs" "$DOTSPACEMACS"
+
+# Install spacemacs
+$GIT_CMD clone --recursive --branch develop "$SPACEMACS_REPO" "$SPACEMACS_DIR"
+$EMACS_CMD
