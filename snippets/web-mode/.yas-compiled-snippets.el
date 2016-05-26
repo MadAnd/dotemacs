5a0d5573236810ec6ec0aad98fc0bcaeddefe545
MadAnd <dev@madand.net>
2016-05-26 03:03:35 +0300

    Update snippets

--- snippets/web-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `web-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'web-mode
-                     '(("pec" "<?= $0 ?>" "<?= ... ?>" nil nil nil "/home/madand/.emacs/snippets/web-mode/<?= ... ?>" nil nil)))
-
-
-;;; Do not edit! File generated at Fri May 20 18:02:05 2016
