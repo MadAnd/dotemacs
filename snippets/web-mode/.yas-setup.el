b045540e35699d5e748d8d166f66ce0af690ee7f
madand <dev@madand.net>
2019-06-12 17:31:04 +0300

    Snippet updates

--- snippets/web-mode/.yas-setup.el
+++ snippets/web-mode/.yas-setup.el
@@ -1,3 +1,4 @@
 (defun yas--web-mode-django-p ()
   "Predicate for checking whether the current buffer is a django template."
-  (and (eq major-mode 'web-mode) (string= web-mode-engine "django")))
+  (when (and (eq major-mode 'web-mode) (string= web-mode-engine "django"))
+    'force-in-comment))
