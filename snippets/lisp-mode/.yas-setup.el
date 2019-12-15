6333830e75586ef8e0a63bb0c6a49d547ba9a5ea
madand <dev@madand.net>
2019-12-15 04:50:19 +0200

    [snippets] CL and Elisp snippets

---
+++ snippets/lisp-mode/.yas-setup.el
@@ -0,0 +1,22 @@
+(require 'thingatpt)
+
+(defun madand//yas-get-parent-defun-name ()
+  (unless (derived-mode-p 'lisp-mode)
+    (error "This snippet works only in lisp-mode buffers."))
+  (setq temp-buffer (current-buffer))
+  (save-excursion
+    (save-restriction
+      (widen)
+      (beginning-of-defun)
+      (evil-forward-WORD-begin)
+      (thing-at-point 'symbol :no-text-props))))
+
+(defun madand//yas-lisp-infer-package-name ()
+  "Infer current package name form file name and path."
+  (if (projectile-project-p)
+      (thread-last buffer-file-name
+        (string-remove-prefix (projectile-project-root))
+        (string-remove-prefix "src/")
+        (string-remove-suffix ".lisp")
+        (concat (projectile-project-name) "/"))
+    ""))
